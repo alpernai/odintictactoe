06824ba61755ab058b6346e309fb4e102c5d7a03
Alejandro Perez Naitoh <alejandropereznaitoh@192.168.1.142>
2024-12-18 18:31:24 -0600

    Create gameBoard, gameFlow, players and manageDOM factory function skeletons.

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("hi");
+const gameBoard = () => {
+    let board = [];
+
+    const createBoard = () => {
+        board = Array(9).fill("");
+    };
+
+    const getBoard = () => board;
+    const placeMark = () => {
+    };
+
+    return { createBoard, getBoard, placeMark };
+}
+
+const gameFlow = () => {
+    let gameOver = false;
+    let currentPlayer = "";
+
+    const startGame = () => {
+    };
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const checkWinner = () => {
+    };
+
+    const switchTurn = () => {
+    };
+
+    const resetGame = () => {
+    };
+
+    return { startGame, getCurrentPlayer, checkWinner, switchTurn, resetGame };
+}
+
+const players = () => {
+    const createPlayer = () => {
+    };
+
+    const getPlayerMark = () => {
+    };
+
+    return { createPlayer, getPlayerMark}
+}
+
+const manageDOM = () => {
+
+    const displayTurn = () => {
+    };
+
+    const displayScore = () => {
+    };
+
+    const displayWinner = () => {
+    };
+
+    return { displayTurn, displayScore, displayWinner }
+}
+
