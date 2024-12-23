10ee4ebe4bf6f3644b053600eab4d91391a7bf6c
Alejandro Perez Naitoh <alejandropereznaitoh@APN-MacBook.local>
2024-12-23 10:15:27 -0600

    Create manageGame factory function skeleton. Initialize and assign gameOver and winningCombination variables. Initialize currentPlayer variable. Create checkWin, changeTurn and increaseScore method skeletons.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = () => {
+const gameBoard = (() => {
     let board = Array(9).fill("");
 
     const resetBoard = () => {
@@ -16,25 +16,56 @@ const gameBoard = () => {
         }
 
         board[index] = mark;
+        console.log(board);
     };
 
     return {resetBoard, getBoard, setMark};
-}
+})();
 
-const createPlayers = () =>{
-    const createPlayer = (mark) => {
-        const player = mark;
-        return player;
+const createPlayer = (name, mark) => {
+    return { name, mark, turn: false, score: 0 };
+};
+
+const manageGame = () => {
+    let gameOver = false;
+    let currentPlayer;
+    const winningCombinations = [
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6]
+    ];    
+
+    const checkWin = () => {
     };
-}
 
-let game = gameBoard();
+    const changeTurn = () => {
+    };
 
-game.setMark('O', "0");
-game.setMark('O', "1");
-game.setMark('O', "2");
-game.setMark('X', "-1");
-game.setMark('X', "9");
+    const increaseScore = () => {
+    };
+
+    return { checkWin, changeTurn, increaseScore };
+};
+
+
+const playerX = createPlayer('Player 1', 'X');
+const playerO = createPlayer('Player 2', 'O');
+
+
+
+
+let game = gameBoard;
+
+game.setMark(playerX.mark, "0");
+game.setMark(playerX.mark, "1");
+game.setMark(playerX.mark, "2");
+game.setMark(playerO.mark, "-1");
+game.setMark(playerX.mark, "9");
 
 
 
