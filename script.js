5367999f2b557f7def0fe8e99365364148d628e7
Alejandro Perez Naitoh <alejandropereznaitoh@APN-MacBook.local>
2025-02-10 12:15:10 -0600

    Finished functioning game with interactive UI. Could add input to add names. Might come back later.

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
+//------------------------------Game Board---------------------------------------------
+
 const gameBoard = (changeTurn, checkWin, checkTie, isGameOver) => {
-    let board = Array(9).fill(""); 
+    let updateDOM = null;
+    let board = Array(9).fill("");              
 
     const getBoard = () => board;  
 
     const resetBoard = () => {
         board = Array(9).fill("");
-        console.log(getBoard());
     };
 
     const setMark = (index, mark) => {
-        if (board[index] !== "") {
-            console.log("Invalid move. Cell marked")
-        }
-
-        if (game.isGameOver()) {
-            console.log("Cannot place mark after game is over")
-            return;
-        }
-
-        if (board[index] === "" && !game.isGameOver()) {  
+        if (board[index] === "" && !isGameOver()) { // Check cell is empty and game isn't over  
             board[index] = mark;  
             changeTurn();         
             checkWin();           
-            checkTie();          
+            checkTie();    
+            if (updateDOM) {
+                updateDOM.displayMessage();
+                updateDOM.displayScores();
+            }  
         }
+    };
+
+    const setUpdateDOM = (dom) => {
+        updateDOM = dom;
     };
     
     return {
         resetBoard,
         getBoard,
         setMark,
+        setUpdateDOM
     };
 };
+
+//------------------------------Players----------------------------------------------
 
 const createPlayer = (mark) => {
     return { mark, score: 0 };
 };
 
+//----------------------------Manage Game------------------------------------------------
+
 const manageGame = (myGameBoard) => {
     let gameOver = false;
+    let isTie = false;
     let winner = "";
     let roundsPlayed = 0;
     let ties = 0;
@@ -60,7 +67,15 @@ const manageGame = (myGameBoard) => {
 
     const getPlayerScore = (player) => player.score; 
 
+    const getRoundsPlayed = () => roundsPlayed; 
+
+    const getTies = () => ties; 
+
     const isGameOver = () => gameOver;
+
+    const getIsTie = () => isTie;
+
+    const getWinner = () => winner;
 
     const checkWin = () => {
         const board = myGameBoard.getBoard();
@@ -71,11 +86,7 @@ const manageGame = (myGameBoard) => {
                 roundsPlayed++;
                 winner = board[a] === 'O' ? playerX : playerO;
                 increasePlayerScore(winner);
-                console.log(`Player ${winner.mark} wins!`);
-                console.log(`Player X score: ${playerX.score}`);
-                console.log(`Player O score: ${playerO.score}`);
-                console.log(`Ties: ${ties}`);
-                console.log(`Rounds played: ${roundsPlayed}`);
+                console.log("Round over. Press reset to keep playing")
                 return;
             }
         });
@@ -85,9 +96,11 @@ const manageGame = (myGameBoard) => {
         const board = myGameBoard.getBoard();
         if (board.every(cell => cell !== "") && !gameOver) {
             gameOver = true;
+            isTie = true;
+            winner = "nobody"
             ties++;
             roundsPlayed++;
-            console.log("It's a tie!");
+            console.log("Round over. Press reset to keep playing")
             return;
         }
     };
@@ -101,34 +114,48 @@ const manageGame = (myGameBoard) => {
         player.score++;
     };
 
-    const resetRound = () => { 
+    const resetGame = () => { 
         gameOver = false;
-        winner = "TBD";
+        isTie = false;
+        winner = "";
         myGameBoard.resetBoard();
         currentPlayer = playerX;
     };
 
     const quitGame = () => {
         gameOver = false;
+        isTie = false;
         roundsPlayed = 0;
         ties = 0;
         playerX.score = 0;
         playerO.score = 0;
-        winner = "TBD";
+        winner = "";
         myGameBoard.resetBoard();
         currentPlayer = playerX;
+
+        console.log(`Is game over? ${isGameOver()}`);
+        console.log(`Winner: ${getWinner()}`);
+        console.log(`Current player: ${getCurrentPlayer().mark}`)
+        console.log(`Player X score: ${playerX.score}`);
+        console.log(`Player O score: ${playerO.score}`);
+        console.log(`Ties: ${ties}`);
+        console.log(`Rounds played: ${roundsPlayed}`);
     }
 
     return {
         getCurrentPlayer,
         getPlayerScore,
+        getRoundsPlayed,
+        getTies,
         isGameOver,
-        resetRound,
+        getWinner,
+        resetGame,
         checkWin,
         checkTie,
         changeTurn,
         increasePlayerScore,
         quitGame, 
+        getIsTie,
         gameOver,
         winner, 
         roundsPlayed,
@@ -140,8 +167,10 @@ const manageGame = (myGameBoard) => {
     };  
 };
 
-const manageDOM = (theGameBoard, manageTheGame) => {    
-    const generateBoard = () => {    
+//------------------------------Manage DOM----------------------------------------------
+
+const manageDOM = (myGameBoard, game) => {    
+    const generateBoard = () => {
         for (let i = 0; i < 9; i++) {
             const gameBoardElement = document.getElementById('game-board')
             const cell = document.createElement('div');            
@@ -166,23 +195,68 @@ const manageDOM = (theGameBoard, manageTheGame) => {
         });
     };
 
+    const displayMessage = () => {
+        let displayMessageElement = document.getElementById('display-message');
+        
+        if (game.getIsTie()) {
+            displayMessageElement.textContent = `It's a tie!`
+        }
+
+        if (game.isGameOver() && !game.getIsTie()) {
+            displayMessageElement.textContent = `Player ${game.getCurrentPlayer().mark} wins!`
+        }
+        
+        if (!game.isGameOver()) {
+            displayMessageElement.textContent = `Player ${game.getCurrentPlayer().mark}'s turn`
+        }
+    }
+
     const displayScores = () => {
         const xScore = document.getElementById('x-score');
         const oScore = document.getElementById('o-score');
         const ties = document.getElementById('ties');
+        const rounds = document.getElementById('rounds');
 
         xScore.textContent = `Player X: ${game.getPlayerScore(game.playerX)}`;
         oScore.textContent = `Player O: ${game.getPlayerScore(game.playerO)}`;
-        ties.textContent = `Ties: ${game.getPlayerScore(game.ties)}`;
+        ties.textContent = `Ties: ${game.getTies()}`;
+        rounds.textContent = `Rounds: ${game.getRoundsPlayed()}`;
+    };
 
+    const runResetGame = () => {
+        const resetButton = document.getElementById('reset-button');
+        const gameBoardElement = document.getElementById('game-board');
+        resetButton.addEventListener('click', (event) => {
+            game.resetGame();
+            gameBoardElement.textContent = "";
+            generateBoard();
+            handleCellClick();
+        })
+    };
+
+    const runQuitGame = () => {
+        const quitButton = document.getElementById('quit-button');
+        const gameBoardElement = document.getElementById('game-board');
+        quitButton.addEventListener('click', (event) => {
+            game.quitGame();
+            gameBoardElement.textContent = "";
+            generateBoard();
+            handleCellClick();
+            displayMessage();
+            displayScores();
+        })
     };
     
     return {generateBoard,
             handleCellClick,
+            displayMessage,
             displayScores,
+            runResetGame,
+            runQuitGame
     };
 };
 
+//--------------------------------Execution--------------------------------------------
 
 const myGameBoard = gameBoard(
     (index) => game.changeTurn(), 
@@ -190,10 +264,14 @@ const myGameBoard = gameBoard(
     () => game.checkTie(),
     () => game.isGameOver()
 );
-
 const game = manageGame(myGameBoard);
 const controlDOM = manageDOM(myGameBoard, game);
+myGameBoard.setUpdateDOM(controlDOM);
 controlDOM.generateBoard();
 controlDOM.handleCellClick();
+controlDOM.displayMessage();
 controlDOM.displayScores();
+controlDOM.runResetGame();
+controlDOM.runQuitGame();
+
 
